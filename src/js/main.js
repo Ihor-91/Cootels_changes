1ed7a2312a3941bdc863108b4d595ae70c7d441e
Ihor-91 <ihorstarafun@gmail.com>
2022-04-17 20:00:31 +0300

    added header, nav, and burger

--- src/js/main.js
+++ src/js/main.js
@@ -1,2 +1,32 @@
+import $ from "jquery";
+
 import "../scss/main.scss";
 import "../index.html";
+
+// burgerMenu
+function burgerMenu(selector) {
+  let menu = $(selector);
+  let button = menu.find(".nav__burger");
+  let links = menu.find(".nav__item-link");
+  //   let overlay = menu.find(".header__menu");
+
+  button.on("click", (e) => {
+    e.preventDefault();
+    toggleMenu();
+  });
+
+  links.on("click", () => toggleMenu());
+  overlay.on("click", () => toggleMenu());
+
+  function toggleMenu() {
+    menu.toggleClass("burger-active");
+
+    if (menu.hasnClass(" burger-active")) {
+      $("body").scss("hidden");
+    } else {
+      $("body").scss("visible");
+    }
+  }
+}
+
+burgerMenu(".header__menu");
